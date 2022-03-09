84d17157d4dcd5b918a6ca92f4812da850243494
MattMyers204453 <mattbmyers@gmail.com>
2022-03-09 15:32:13 -0700

    pics complete

--- client/src/Gallery.js
+++ client/src/Gallery.js
@@ -4,12 +4,29 @@ import './Gallery.scss';
 
 const images = [
     {
-        original: 'https://i.ibb.co/j5cxRsY/img-0159-1.jpg',
-        thumbnail: 'https://i.ibb.co/j5cxRsY/img-0159-1.jpg',
+        original: 'https://i.ibb.co/hDbw4Hr/merge1.jpg',
+        thumbnail: 'https://i.ibb.co/hDbw4Hr/merge1.jpg',
     },
     {
-        original: 'https://picsum.photos/id/1015/1000/600/',
+        original: 'https://i.ibb.co/ZK8sBRh/merge2.jpg',
+        thumbnail: 'https://i.ibb.co/ZK8sBRh/merge2.jpg',
     },
+    {
+        original: 'https://i.ibb.co/WsGpZxD/IMG-0161-1.jpg',
+        thumbnail: 'https://i.ibb.co/WsGpZxD/IMG-0161-1.jpg',
+    },
+    {
+        original: 'https://i.ibb.co/DYKhD5B/merge3.jpg',
+        thumbnail: 'https://i.ibb.co/DYKhD5B/merge3.jpg',
+    },
+    {
+        original: 'https://i.ibb.co/ScZhxD1/merge4.jpg',
+        thumbnail: 'https://i.ibb.co/ScZhxD1/merge4.jpg',
+    },
+    {
+        original: 'https://i.ibb.co/F0tLvNP/merge5.jpg',
+        thumbnail: 'https://i.ibb.co/F0tLvNP/merge5.jpg',
+    }
 ];
 
 class Gallery extends Component {
